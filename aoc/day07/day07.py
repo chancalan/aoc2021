a22dfd91ad89360336c65f652acd8c3602911213
TheTeaRex <thetearex10@gmail.com>
2023-12-01 21:53:31 +0000

    aoc2021 day07

--- aoc/day07/day07.py
+++ aoc/day07/day07.py
@@ -10,12 +10,37 @@ class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         # sets self.input to the provided input
         super(Solver, self).__init__(input)
-        self.input = self.input.splitlines()
+        self.input = sorted([int(i) for i in self.input.split(',')])
+        self.cache = [0 for _ in range(2000)]
+        self.cache[0] = 1
+        self.last_seen = 1
+
+    def calculate_fuel_need(self, num: int):
+        if self.cache[num - 1] != 0:
+            return self.cache[num - 1]
+
+        result = self.cache[self.last_seen - 1]
+        for i in range(self.last_seen + 1, num + 1):
+            result += i
+            self.cache[i - 1] = result
+        self.last_seen = num
+        return result
 
     def part_one(self) -> int:
-        # TODO: actually return the answer
-        return 0
+        result = float('inf')
+        for i in range(self.input[0], self.input[-1] + 1):
+            temp = 0
+            for crab_pos in self.input:
+                temp += abs(crab_pos - i)
+            result = min(result, temp)
+        return result
 
     def part_two(self) -> int:
-        # TODO: actually return the answer
-        return 0
+        result = float('inf')
+        for i in range(self.input[0], self.input[-1] + 1):
+            temp = 0
+            for crab_pos in self.input:
+                temp += self.calculate_fuel_need(abs(crab_pos - i))
+            result = min(result, temp)
+
+        return result
